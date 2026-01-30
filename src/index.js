d5a8f4b6f320e76edb52ba143e6982dfccddb21c
Chi-Chun Chang <cjchang925@gmail.com>
2026-01-30 21:15:04 +0800

    feat: upgrade dependencies and update to React 18.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import reportWebVitals from './reportWebVitals';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <GoogleOAuthProvider clientId="814398908829-ev3belrqio4b67tp8anlhut0iihr5pb4.apps.googleusercontent.com">
     <App />
-  </GoogleOAuthProvider>,
-  document.getElementById('root')
+  </GoogleOAuthProvider>
 );
 
 reportWebVitals();
